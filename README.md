f1b7f25e1214163bed6d20d375e22c3ce42e60e3
Akshay Gopinath <akshay8055.007@gmail.com>
2023-05-29 19:55:45 +0100

    add: script and readme draft

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# framework-pop-os-22.04
-Setting up Pop!_OS 22.04 on Framework 12th Gen laptop
+# Pop!_OS 22.04 - Framework 12th Gen Laptop
+
+This repo is just to provide a quick guide/script to set up Pop!_OS 22.04 on the Framework Laptop, this is for the 12th gen motherboard.
+
+
---
+++ pop-script.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+sudo apt update && sudo apt upgrade -y && echo "options snd-hda-intel model=dell-headset-multi" | sudo tee -a /etc/modprobe.d/alsa-base.conf && echo "blacklist hid_sensor_hub" | sudo tee -a /etc/modprobe.d/framework-als-blacklist.conf && sudo update-initramfs -u && sudo kernelstub -a nvme.noacpi=1 && sudo kernelstub -a i915.enable_psr=1 && sudo apt remove xserver-xorg-video-intel && echo "export MOZ_USE_XINPUT2=1" >> ~/.profile
+
+# ask for y/n confirmation, if yes add resolution to .profile
+read -p "Do you want to use X11 and use reduced resultion? (y/n)" answer
+case ${answer:0:1} in
+    y|Y )
+        xrandr --newmode "1692x1128_60.00"  159.25  1692 1808 1984 2272  1128 1131 1141 1170 -hsync +vsync && xrandr --addmode eDP-1 "1692x1128_60.00" && echo "xrandr --newmode "1692x1128_60.00"  159.25  1692 1808 1984 2272  1128 1131 1141 1170 -hsync +vsync" >> ~/.profile && echo "xrandr --addmode eDP-1 "1692x1128_60.00"" >> ~/.profile && echo -e "\e[31m[INFO] Change Display Resolution to 1692x1128 in System Settings\e[0m\n"
+    ;;
+    * )
+        # output red colour hello text
+        sudo sed -i '7s/.*/#WaylandEnable=false/' /etc/gdm3/custom.conf &&
+        echo -e "\e[31m[INFO] Please use Wayland if you selected no\e[0m"
+    ;;
+esac
+
+sudo apt install fprintd libpam-fprintd && echo -e "\e[31m[INFO] Please reboot!\nIf you selected not to use X11, select Wayland when at the login prompt, and use fractional scaling at 150% for the best experience\e[0m"
