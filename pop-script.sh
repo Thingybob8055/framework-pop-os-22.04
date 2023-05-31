a98e1479ea4986b3d23b042a5df671f268253028
Akshay Gopinath <akshay8055.007@gmail.com>
2023-05-31 21:57:02 +0100

    remove: boiler plate comment

--- pop-script.sh
+++ pop-script.sh
@@ -9,7 +9,6 @@ case ${answer:0:1} in
         xrandr --newmode "1692x1128_60.00"  159.25  1692 1808 1984 2272  1128 1131 1141 1170 -hsync +vsync && xrandr --addmode eDP-1 "1692x1128_60.00" && echo "xrandr --newmode "1692x1128_60.00"  159.25  1692 1808 1984 2272  1128 1131 1141 1170 -hsync +vsync" >> ~/.profile && echo "xrandr --addmode eDP-1 "1692x1128_60.00"" >> ~/.profile && echo -e "\e[31m[INFO] Change Display Resolution to 1692x1128 in System Settings\e[0m\n" && echo "export MOZ_USE_XINPUT2=1" >> ~/.profile
     ;;
     * )
-        # output red colour hello text
         sudo sed -i '7s/.*/#WaylandEnable=false/' /etc/gdm3/custom.conf && echo "export MOZ_ENABLE_WAYLAND=1" >> ~/.profile &&
         echo -e "\e[31m[INFO] Please use Wayland if you selected no\e[0m"
     ;;
